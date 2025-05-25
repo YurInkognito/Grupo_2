c74ff5b46fcf406d32b935abd4a827b84c93ffb9
Rafael Lima <95830415+rafahgol@users.noreply.github.com>
2025-05-25 16:37:02 -0300

    Update config.gd

--- scripts/config.gd
+++ scripts/config.gd
@@ -3,13 +3,6 @@ extends Control
 func _ready() -> void:
 	pause_game()
 func _on_button_back_pressed() -> void:
-	if get_parent().name == "TelaTitulo":
-		print("Back to Menu")
-		get_parent().get_child(0).visible = true
-		unpause_game()
-		self.queue_free()
-	else:
-		print("Baco to Game")
 		unpause_game()
 		self.queue_free()
 
